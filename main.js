5e5032abb8ec4cd888b237f57a4863b79b420899
yuk7 <yukx00@gmail.com>
2024-02-26 05:21:53 +0900

    main.js: refactor

--- main.js
+++ main.js
@@ -30,21 +30,7 @@ function calculateCommandStartLocation(outputCh) {
     return outputCh * (inputChCnt + 4);
 }
 
-async function sendChUpDownCommand(outputCh, isUp) {
-    if (outputCh < 0 || outputCh >= outputChCnt) {
-        alert('Invalid channel number');
-        return;
-    }
-
-    let startLocation = calculateCommandStartLocation(outputCh) - 3;
-    let commandLocation = startLocation + (isUp ? 1 : 0);
-
-    if (commandLocation < 0) {
-        commandLocation += calculateCommandStartLocation(outputChCnt);
-    }
-
-    let commandStr = `cir ${('0' + (Number(commandLocation).toString(16))).slice(-2)}\r\n`
-
+async function sendCommand(commandStr) {
     try {
         const encoder = new TextEncoder();
         const writer = port.writable.getWriter();
@@ -53,6 +39,23 @@ async function sendChUpDownCommand(outputCh, isUp) {
     } catch (error) {
         alert('Error: ' + error);
     }
+}
+
+async function sendChUpDownCommand(outputCh, isUp) {
+    if (outputCh < 0 || outputCh >= outputChCnt) {
+        alert('Invalid channel number');
+        return;
+    }
+
+    const startLocation = calculateCommandStartLocation(outputCh) - 3;
+
+    let commandLocation = startLocation + (isUp ? 1 : 0);
+    if (commandLocation < 0) {
+        commandLocation += calculateCommandStartLocation(outputChCnt);
+    }
+
+    const commandStr = `cir ${('0' + (Number(commandLocation).toString(16))).slice(-2)}\r\n`
+    await sendCommand(commandStr);
 }
 
 async function sendChCommand(inputCh, outputCh) {
@@ -60,58 +63,50 @@ async function sendChCommand(inputCh, outputCh) {
         alert('Invalid channel number');
         return;
     }
-    let commandLocation = calculateCommandStartLocation(outputCh) + inputCh;
-    let commandStr = `cir ${('0' + (Number(commandLocation).toString(16))).slice(-2)}\r\n`
-
-    try {
-        const encoder = new TextEncoder();
-        const writer = port.writable.getWriter();
-        await writer.write(encoder.encode(commandStr));
-        writer.releaseLock();
-    } catch (error) {
-        alert('Error: ' + error);
-    }
+    const commandLocation = calculateCommandStartLocation(outputCh) + inputCh;
+    const commandStr = `cir ${('0' + (Number(commandLocation).toString(16))).slice(-2)}\r\n`
+    await sendCommand(commandStr);
 }
 
 window.addEventListener('load', function () {
-    let controllerTable = document.getElementById('controller-table-area');
-    let table = document.createElement('table');
-    let tbody = document.createElement('tbody');
+    const controllerTable = document.getElementById('controller-table-area');
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
 
-    let inputLabelsTr = document.createElement('tr');
+    const inputLabelsTr = document.createElement('tr');
     inputLabelsTr.appendChild(document.createElement('th'));
-    let inLabel = document.createElement('th');
+    const inLabel = document.createElement('th');
     inLabel.textContent = 'In';
     inputLabelsTr.appendChild(inLabel);
     for (let i = 0; i < inputChCnt; i++) {
-        let th = document.createElement('th');
+        const th = document.createElement('th');
         th.textContent = i + 1;
         inputLabelsTr.appendChild(th);
     }
-    let upLabel = document.createElement('th');
+    const upLabel = document.createElement('th');
     upLabel.textContent = '+';
     inputLabelsTr.appendChild(upLabel);
-    let downLabel = document.createElement('th');
+    const downLabel = document.createElement('th');
     downLabel.textContent = '-';
     inputLabelsTr.appendChild(downLabel);
 
     tbody.appendChild(inputLabelsTr);
 
-    let outputLabelsTr = document.createElement('tr');
-    let outLabel = document.createElement('th');
+    const outputLabelsTr = document.createElement('tr');
+    const outLabel = document.createElement('th');
     outLabel.textContent = 'Out';
     outputLabelsTr.appendChild(outLabel);
     tbody.appendChild(outputLabelsTr);
 
     for (let outCount = 0; outCount < outputChCnt; outCount++) {
-        let tr = document.createElement('tr');
-        let outChannelLabel = document.createElement('th');
+        const tr = document.createElement('tr');
+        const outChannelLabel = document.createElement('th');
         outChannelLabel.textContent = outCount + 1;
         tr.appendChild(outChannelLabel);
         tr.appendChild(document.createElement('th'));
         for (let inCount = 0; inCount < inputChCnt; inCount++) {
-            let th = document.createElement('th');
-            let button = document.createElement('input');
+            const th = document.createElement('th');
+            const button = document.createElement('input');
             button.type = 'button';
             button.value = (inCount + 1) + '->' + (outCount + 1);
             button.onclick = (function (_inCount, _outCount) {
@@ -123,8 +118,8 @@ window.addEventListener('load', function () {
             tr.appendChild(th);
         }
 
-        let upButton = document.createElement('th');
-        let upButtonEl = document.createElement('input');
+        const upButton = document.createElement('th');
+        const upButtonEl = document.createElement('input');
         upButtonEl.type = 'button';
         upButtonEl.value = '+';
         upButtonEl.onclick = (function (_outCount) {
@@ -135,8 +130,8 @@ window.addEventListener('load', function () {
         upButton.appendChild(upButtonEl);
         tr.appendChild(upButton);
 
-        let downButton = document.createElement('th');
-        let downButtonEl = document.createElement('input');
+        const downButton = document.createElement('th');
+        const downButtonEl = document.createElement('input');
         downButtonEl.type = 'button';
         downButtonEl.value = '-';
         downButtonEl.onclick = (function (_outCount) {
@@ -149,7 +144,6 @@ window.addEventListener('load', function () {
 
         tbody.appendChild(tr);
     }
-
     table.appendChild(tbody);
     controllerTable.appendChild(table);
 });
